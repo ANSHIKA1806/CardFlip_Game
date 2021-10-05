fdf1d7b112178afcb831fdbabee459ca67fc1f96
daniellycosta <daniellycmcosta@gmail.com>
2021-10-05 15:24:12 -0300

    fix: update index

--- js/index.js
+++ js/index.js
@@ -1,36 +1,52 @@
 const deckCards = [
-  'Agility.png',
-  'Agility.png',
-  'Boat.png',
-  'Boat.png',
-  'Citizenship.png',
-  'Citizenship.png',
-  'Hack.png',
-  'Hack.png',
-  'Nerd-Rage.png',
-  'Nerd-Rage.png',
-  'Nuka-Cola.png',
-  'Nuka-Cola.png',
-  'Robotics.png',
-  'Robotics.png',
-  'Shock.png',
-  'Shock.png',
+  "Agility.png",
+  "Agility.png",
+  "Boat.png",
+  "Boat.png",
+  "Citizenship.png",
+  "Citizenship.png",
+  "Hack.png",
+  "Hack.png",
+  "Nerd-Rage.png",
+  "Nerd-Rage.png",
+  "Nuka-Cola.png",
+  "Nuka-Cola.png",
+  "Robotics.png",
+  "Robotics.png",
+  "Shock.png",
+  "Shock.png",
 ];
-const deck = document.querySelector('.deck');
+
+const deck = document.querySelector(".deck");
 let opened = [];
 let matched = [];
-const modal = document.getElementById('modal');
-const reset = document.querySelector('.reset-btn');
-const playAgain = document.querySelector('.play-again-btn');
-const movesCount = document.querySelector('.moves-counter');
+let gameMode = "";
+
+const modal = document.getElementById("modal");
+const startModal = document.getElementById("start-modal");
+
+const reset = document.querySelector(".reset-btn");
+const playAgain = document.getElementById("play-again");
+const movesCount = document.querySelector(".moves-counter");
+
+const easyGame = document.getElementById("easyGame");
+const mediumGame = document.getElementById("mediumGame");
+const hardGame = document.getElementById("hardGame");
+
 let moves = 0;
-const star = document.getElementById('star-rating').querySelectorAll('.star');
+const star = document.getElementById("star-rating").querySelectorAll(".star");
 let starCount = 3;
-const timeCounter = document.querySelector('.timer');
+const timeCounter = document.querySelector(".timer");
 let time;
 let minutes = 0;
 let seconds = 0;
 let timeStart = false;
+
+function showModalStart() {
+  startModal.style.display = "block";
+}
+
+showModalStart();
 
 function shuffle(array) {
   let currentIndex = array.length,
@@ -48,30 +64,43 @@ function shuffle(array) {
 }
 
 function startGame() {
+  let cards = [];
+  console.log(gameMode, mediumGame);
+  if (gameMode === "easy") {
+    cards = deckCards.slice(0, 4);
+    deck.classList.add("easy");
+  } else if (gameMode === "medium") {
+    deck.classList.add("medium");
+    cards = deckCards.slice(0, 8);
+  } else {
+    deck.classList.add("hard");
+    cards = [...deckCards];
+  }
+
   // Invoke shuffle function and store in variable
-  const shuffledDeck = shuffle(deckCards);
+  const shuffledDeck = shuffle(cards);
   // Iterate over deck of cards array
   for (let i = 0; i < shuffledDeck.length; i++) {
     // Create the <li> tags
-    const liTag = document.createElement('LI');
+    const liTag = document.createElement("LI");
     // Give <li> class of card
-    liTag.classList.add('card');
+    liTag.classList.add("card");
     // Create the <img> tags
-    const addImage = document.createElement('IMG');
+    const addImage = document.createElement("IMG");
     // Append <img> to <li>
     liTag.appendChild(addImage);
     // Set the img src path with the shuffled deck
-    addImage.setAttribute('src', 'img/' + shuffledDeck[i]);
+    addImage.setAttribute("src", "img/" + shuffledDeck[i]);
     // Add an alt tag to the image
-    addImage.setAttribute('alt', 'image of vault boy from fallout');
+    addImage.setAttribute("alt", "image of vault boy from fallout");
     // Update the new <li> to the deck <ul>
 
-    const divTag = document.createElement('DIV');
-    divTag.classList.add('tilt');
+    const divTag = document.createElement("DIV");
+    divTag.classList.add("tilt");
     divTag.appendChild(liTag);
 
     updateReflection(liTag, 100, 0);
-    liTag.addEventListener('mousemove', (event) => {
+    liTag.addEventListener("mousemove", (event) => {
       const scale = 0.03;
       const midX = (liTag.clientHeight / 2) * scale;
       const mouseXoffset = event.offsetX * scale;
@@ -85,20 +114,18 @@ function startGame() {
       liTag.style.transform = rotation;
     });
 
-    liTag.addEventListener('mouseleave', (event) => {
+    liTag.addEventListener("mouseleave", (event) => {
       // liTag.style.transform = `rotateX(0deg) rotateY(0deg)`;
     });
 
     // flip liTag on click
-    liTag.addEventListener('click', (event) => {
+    liTag.addEventListener("click", (event) => {
       liTag.style.transform = `rotateX(0deg) rotateY(180deg)`;
     });
 
     deck.appendChild(divTag);
   }
 }
-
-startGame();
 
 function removeCard() {
   // As long as <ul> deck has a child node, remove it
@@ -117,12 +144,12 @@ function timer() {
     }
     // Update the timer in HTML with the time it takes the user to play the game
     timeCounter.innerHTML =
-      minutes.toLocaleString('en-US', {
+      minutes.toLocaleString("en-US", {
         minimumIntegerDigits: 2,
         useGrouping: false,
       }) +
-      ':' +
-      seconds.toLocaleString('en-US', {
+      ":" +
+      seconds.toLocaleString("en-US", {
         minimumIntegerDigits: 2,
         useGrouping: false,
       });
@@ -139,10 +166,10 @@ function resetEverything() {
   timeStart = false;
   seconds = 0;
   minutes = 0;
-  timeCounter.innerHTML = '00:00';
+  timeCounter.innerHTML = "00:00";
   // Reset star count and the add the class back to show stars again
-  star[1].firstElementChild.classList.add('fa-star');
-  star[2].firstElementChild.classList.add('fa-star');
+  star[1].firstElementChild.classList.add("fa-star");
+  star[2].firstElementChild.classList.add("fa-star");
   starCount = 3;
   // Reset moves count and reset its inner HTML
   moves = 0;
@@ -152,8 +179,11 @@ function resetEverything() {
   opened = [];
   // Clear the deck
   removeCard();
-  // Create a new deck
-  startGame();
+
+  if (gameMode === "easy") deck.classList.remove("easy");
+  else if (gameMode === "medium") deck.classList.remove("medium");
+  else deck.classList.remove("hard");
+  startModal.style.display = "block";
 }
 
 function movesCounter() {
@@ -166,11 +196,11 @@ function movesCounter() {
 function starRating() {
   if (moves === 14) {
     // First element child is the <i> within the <li>
-    star[2].firstElementChild.classList.remove('fa-star');
+    star[2].firstElementChild.classList.remove("fa-star");
     starCount--;
   }
   if (moves === 18) {
-    star[1].firstElementChild.classList.remove('fa-star');
+    star[1].firstElementChild.classList.remove("fa-star");
     starCount--;
   }
 }
@@ -179,7 +209,7 @@ function compareTwo() {
   // When there are 2 cards in the opened array
   if (opened.length === 2) {
     // Disable any further mouse clicks on other cards
-    document.body.style.pointerEvents = 'none';
+    document.body.style.pointerEvents = "none";
   }
   // Compare the two images src
   if (opened.length === 2 && opened[0].src === opened[1].src) {
@@ -198,12 +228,12 @@ function match() {
   the class of match to the imgages parent: the <li> tag
   */
   setTimeout(function () {
-    opened[0].parentElement.classList.add('match');
-    opened[1].parentElement.classList.add('match');
+    opened[0].parentElement.classList.add("match");
+    opened[1].parentElement.classList.add("match");
     // Push the matched cards to the matched array
     matched.push(...opened);
     // Allow for further mouse clicks on cards
-    document.body.style.pointerEvents = 'auto';
+    document.body.style.pointerEvents = "auto";
     // Check to see if the game has been won with all 8 pairs
     winGame();
     // Clear the opened array
@@ -219,10 +249,10 @@ function noMatch() {
   the class of flip removed from the images parent element <li>*/
   setTimeout(function () {
     // Remove class flip on images parent element
-    opened[0].parentElement.classList.remove('flip');
-    opened[1].parentElement.classList.remove('flip');
+    opened[0].parentElement.classList.remove("flip");
+    opened[1].parentElement.classList.remove("flip");
     // Allow further mouse clicks on cards
-    document.body.style.pointerEvents = 'auto';
+    document.body.style.pointerEvents = "auto";
     // Remove the cards from opened array
     opened = [];
   }, 700);
@@ -233,53 +263,57 @@ function noMatch() {
 
 function AddStats() {
   // Access the modal content div
-  const stats = document.querySelector('.modal-content');
+  const stats = document.getElementById("win-modal-content");
   // Create three different paragraphs
   for (let i = 1; i <= 3; i++) {
     // Create a new Paragraph
-    const statsElement = document.createElement('p');
+    const statsElement = document.createElement("p");
     // Add a class to the new Paragraph
-    statsElement.classList.add('stats');
+    statsElement.classList.add("stats");
     // Add the new created <p> tag to the modal content
     stats.appendChild(statsElement);
   }
   // Select all p tags with the class of stats and update the content
-  let p = stats.querySelectorAll('p.stats');
+  let p = stats.querySelectorAll("p.stats");
   // Set the new <p> to have the content of stats (time, moves and star rating)
   p[0].innerHTML =
-    'Time to complete: ' + minutes + ' Minutes and ' + seconds + ' Seconds';
-  p[1].innerHTML = 'Moves Taken: ' + moves;
-  p[2].innerHTML = 'Your Star Rating is: ' + starCount + '/3';
+    "Time to complete: " + minutes + " Minutes and " + seconds + " Seconds";
+  p[1].innerHTML = "Moves Taken: " + moves;
+  p[2].innerHTML = "Your Star Rating is: " + starCount + "/3";
 }
 
 function displayModal() {
   // Access the modal <span> element (x) that closes the modal
-  const modalClose = document.getElementsByClassName('close')[0];
+  const modalClose = document.getElementsByClassName("close")[0];
   // When the game is won set modal to display block to show it
-  modal.style.display = 'block';
+  modal.style.display = "block";
 
   // When the user clicks on <span> (x), close the modal
   modalClose.onclick = function () {
-    modal.style.display = 'none';
+    modal.style.display = "none";
   };
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.style.display = "none";
     }
   };
 }
 
 function winGame() {
-  if (matched.length === 16) {
+  let len = 0;
+  if (gameMode === "easy") len = 4;
+  else len = gameMode === "medium" ? 8 : 16;
+
+  if (matched.length === len) {
     stopTime();
     AddStats();
     displayModal();
   }
 }
 
-deck.addEventListener('click', function (evt) {
-  if (evt.target.nodeName === 'LI') {
+deck.addEventListener("click", function (evt) {
+  if (evt.target.nodeName === "LI") {
     // Start the timer after the first click of one card
     // Executes the timer() function
     if (timeStart === false) {
@@ -293,7 +327,7 @@ deck.addEventListener('click', function (evt) {
   //Flip the card and display cards img
   function flipCard() {
     // When <li> is clicked add the class .flip to show img
-    evt.target.classList.add('flip');
+    evt.target.classList.add("flip");
     // Call addToOpened() function
     addToOpened();
   }
@@ -312,20 +346,35 @@ deck.addEventListener('click', function (evt) {
   }
 });
 
-reset.addEventListener('click', resetEverything);
+reset.addEventListener("click", resetEverything);
 
-playAgain.addEventListener('click', function () {
-  modal.style.display = 'none';
+easyGame.addEventListener("click", function () {
+  console.log("ENTROU");
+  startModal.style.display = "none";
+  gameMode = "easy";
+  startGame();
+});
+
+mediumGame.addEventListener("click", function () {
+  startModal.style.display = "none";
+  gameMode = "medium";
+  console.log("ENTROU");
+  startGame();
+});
+
+hardGame.addEventListener("click", function () {
+  startModal.style.display = "none";
+  gameMode = "hard";
+  console.log("ENTROU");
+  startGame();
+});
+
+playAgain.addEventListener("click", function () {
+  modal.style.display = "none";
   resetEverything();
 });
 
-// const cards = document.getElementsByClassName('card');
-
-// Array.from(cards).forEach((card) => {
-
-// });
-
 function updateReflection(card, degree, percentage) {
   card.style.background = `linear-gradient(${degree}deg, rgba(23, 180, 109 ,0.6) 0%,rgba(23, 180, 109,0.8) ${percentage}%,rgba(23, 180, 109,0.7) 100%)`;
-  card.style.backgroundSize = 'cover';
+  card.style.backgroundSize = "cover";
 }
